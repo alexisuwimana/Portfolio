61b6954c968375fe055c66acf07745bdd0224915
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-09 11:32:24 -0500

    Fix linter errors on these files.

--- scripts.js
+++ scripts.js
@@ -4,18 +4,18 @@ const humbergerMenu = document.getElementsByClassName('menu');
 const selectMobileMenu = document.getElementsByClassName('mobile_menu');
 const bodySelect = document.querySelector('body');
 
-function toggleMenu (){
-    selectMobileMenu[0].classList.toggle('hidden_class');
-    if(selectMobileMenu[0].classList.contains('hidden_class')){
-        bodySelect.style.overflow = 'auto';
-    }else {
-        bodySelect.style.overflow = 'hidden';
-    }
+function toggleMenu() {
+  selectMobileMenu[0].classList.toggle('hidden_class');
+  if (selectMobileMenu[0].classList.contains('hidden_class')) {
+    bodySelect.style.overflow = 'auto';
+  } else {
+    bodySelect.style.overflow = 'hidden';
+  }
 }
 
-humbergerMenu[0].addEventListener('click',toggleMenu);
+humbergerMenu[0].addEventListener('click', toggleMenu);
 xrosssMenu[0].addEventListener('click', toggleMenu);
 
-menuLink.forEach(link => {
-    link.addEventListener('click',toggleMenu);
+menuLink.forEach((link) => {
+  link.addEventListener('click', toggleMenu);
 });
