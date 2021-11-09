8ec65f43c8d3f0650bca9681cc89d786f82e49b9
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-09 11:14:50 -0500

    Add ability to not navigate on the page unless we use some of our links.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,15 @@ const menuLink = document.querySelectorAll('.mobile_menu > ul > li');
 const xrosssMenu = document.getElementsByClassName('xross_menu');
 const humbergerMenu = document.getElementsByClassName('menu');
 const selectMobileMenu = document.getElementsByClassName('mobile_menu');
+const bodySelect = document.querySelector('body');
 
 function toggleMenu (){
     selectMobileMenu[0].classList.toggle('hidden_class');
+    if(selectMobileMenu[0].classList.contains('hidden_class')){
+        bodySelect.style.overflow = 'auto';
+    }else {
+        bodySelect.style.overflow = 'hidden';
+    }
 }
 
 humbergerMenu[0].addEventListener('click',toggleMenu);
